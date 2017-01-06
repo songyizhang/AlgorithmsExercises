8732688231503931b17a96b728a358e51bd961ce
songyizhang <zhangsongyijianchi@gmail>
2017-01-06 15:38:12 +0800

    first commit

--- src/Ex1_2/Ex1.java
+++ src/Ex1_2/Ex1.java
@@ -7,14 +7,14 @@ import edu.princeton.cs.algs4.StdRandom;
 public class Ex1 {
 	public static void main(String[] args) {
 		int N = Integer.parseInt(args[0]);
-		Point2D[] p = new Point2D[N];
+		Point2D[] points = new Point2D[N];
 		for (int i = 0; i < N; i++) {
-			p[i] = new Point2D(StdRandom.uniform(), StdRandom.uniform());
+			points[i] = new Point2D(StdRandom.uniform(), StdRandom.uniform());
 		}
 		double distance = 2.0;
-		for (int i = 0; i < p.length - 1; i++) {
-			for (int j = i + 1; j < p.length; j++) {
-				double dis = p[i].distanceTo(p[j]);
+		for (int i = 0; i < points.length - 1; i++) {
+			for (int j = i + 1; j < points.length; j++) {
+				double dis = points[i].distanceTo(points[j]);
 				if (dis < distance) {
 					distance = dis;
 				}
