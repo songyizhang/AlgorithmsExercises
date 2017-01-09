0effd0956040f662d4934e74b547b8a8a66936a3
songyizhang <zhangsongyijianchi@gmail>
2017-01-09 15:46:33 +0800

    first commit

---
+++ src/Ex1_2/Ex6.java
@@ -0,0 +1,11 @@
+package Ex1_2;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Ex6 {
+	public static void main(String[] args) {
+		String s = args[0];
+		String t = args[1];
+		StdOut.println(s.length() == t.length() && s.concat(s).indexOf(t) >= 0);
+	}
+}
