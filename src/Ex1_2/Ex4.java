8a1d39cfa002486be79147f9c0d3b410efdb664b
songyizhang <zhangsongyijianchi@gmail>
2017-01-09 15:07:38 +0800

    first commit

--- src/Ex1_2/Ex4.java
+++ src/Ex1_2/Ex4.java
@@ -1,5 +1,13 @@
 package Ex1_2;
 
-public class Ex4 {
+import edu.princeton.cs.algs4.StdOut;
 
+public class Ex4 {
+	public static void main(String[] args) {
+		String string1 = "hello";
+		String string2 = string1;
+		string1 = "world";
+		StdOut.println(string1);
+		StdOut.println(string2);
+	}
 }
