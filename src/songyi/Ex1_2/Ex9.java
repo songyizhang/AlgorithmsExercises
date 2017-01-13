8c89a0a5a77d543de6f7bbdc295e6059e316f680
songyizhang <zhangsongyijianchi@gmail>
2017-01-13 14:06:00 +0800

    first commit

--- src/songyi/Ex1_2/Ex9.java
+++ src/songyi/Ex1_2/Ex9.java
@@ -2,42 +2,38 @@ package songyi.Ex1_2;
 
 import java.util.Arrays;
 
+import edu.princeton.cs.algs4.Counter;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Ex9 {
-	public static void main(String[] args) {
-		In in = new In(args[0]);
-		int[] whitelist = in.readAllInts();
-		Arrays.sort(whitelist);
-		long time = System.currentTimeMillis();
-		while (!StdIn.isEmpty()) {
-			// 读取键值，如果不存在于白名单中则将其打印
-			int key = StdIn.readInt();
-			if (rank(key, whitelist) < 0) {
-				StdOut.println(key);
-			}
-		}
-		long duration = System.currentTimeMillis() - time;
-		StdOut.println("二分查找所需时间为:" + duration + "毫秒");
-	}
-////
-	private static int rank(int key, int[] a) {
+	public static int rank(int key, int[] a, Counter counter) { // 数组必须是有序的
+		counter.increment();
 		int lo = 0;
 		int hi = a.length - 1;
-		while (lo <= hi) {
+		while (lo <= hi) { // 被查找的键要么不存在，要么必然存在于a[lo..hi] 之中
 			int mid = lo + (hi - lo) / 2;
-			if (key < a[mid]) {
+			if (key < a[mid])
 				hi = mid - 1;
-			} else if (key > a[mid]) {
+			else if (key > a[mid])
 				lo = mid + 1;
-			} else {
+			else
 				return mid;
-			}
 		}
 		return -1;
-
 	}
 
+	public static void main(String[] args) {
+		@SuppressWarnings("deprecation")
+		int[] whitelist = In.readInts(args[0]);
+		Arrays.sort(whitelist);
+		Counter counter = new Counter("key");
+		while (!StdIn.isEmpty()) { // 读取键值，如果不存在于白名单中则将其打印
+			int key = StdIn.readInt();
+			if (rank(key, whitelist, counter) < 0)
+				StdOut.println(key);
+		}
+		StdOut.println("counter:" + counter);
+	}
 }
